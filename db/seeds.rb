cdae94fceea6d15efb08ec2f2c6560e3877e1a75
florianrabbah <floflo97231@live.fr>
2023-10-26 22:17:14 +0200

    finish2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require_relative "seeds/doctors"
+require_relative "seeds/patients"
+require_relative "seeds/appointments"
+require_relative "seeds/cities"
+require_relative "seeds/specialities"
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,9 +12,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# db/seeds.rb
-require_relative "seeds/doctors"
-require_relative "seeds/patients"
-require_relative "seeds/appointments"
-require_relative "seeds/cities"
-require_relative "seeds/specialities"
+# db/seeds.r
